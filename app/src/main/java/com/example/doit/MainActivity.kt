5dcac662d177474cc62c4622f5b106c44f3fb193
ImeshaNadeeshani <imeshanadeeshani761@gmail.com>
2024-05-10 20:35:16 +0530

    first commits

---
+++ app/src/main/java/com/example/doit/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.doit
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import net.penguincoders.doit.Adapters.ToDoAdapter
+import net.penguincoders.doit.Model.ToDoModel
+import net.penguincoders.doit.Utils.DatabaseHandler
+
+
+class MainActivity : AppCompatActivity(), DialogCloseListener {
+
+    private lateinit var db: DatabaseHandler
+    private lateinit var tasksRecyclerView: RecyclerView
+    private lateinit var tasksAdapter: ToDoAdapter
+    private lateinit var fab: FloatingActionButton
+    private lateinit var taskList: MutableList<ToDoModel>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        db = DatabaseHandler(this)
+        db.openDatabase()
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView)
+        tasksRecyclerView.layoutManager = LinearLayoutManager(this)
+        tasksAdapter = ToDoAdapter(db, this)
+        tasksRecyclerView.adapter = tasksAdapter
+
+        val itemTouchHelper = ItemTouchHelper(RecyclerItemTouchHelper(tasksAdapter))
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView)
+
+        fab = findViewById(R.id.fab)
+
+        taskList = db.getAllTasks().toMutableList()
+        taskList.reverse()
+
+        tasksAdapter.setTasks(taskList)
+
+        fab.setOnClickListener {
+            AddNewTask.newInstance().show(supportFragmentManager, AddNewTask.TAG)
+        }
+    }
+
+    override fun handleDialogClose(dialog: DialogInterface) {
+        taskList = db.getAllTasks().toMutableList()
+        taskList.reverse()
+        tasksAdapter.setTasks(taskList)
+        tasksAdapter.notifyDataSetChanged()
+    }
+}
