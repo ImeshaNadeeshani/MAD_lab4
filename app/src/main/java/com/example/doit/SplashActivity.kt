704f3bec07689e458c343626761ff724dfae5bd7
ImeshaNadeeshani <imeshanadeeshani761@gmail.com>
2024-05-12 11:08:04 +0530

    main,splash,TodoAdapter_changes1

--- app/src/main/java/com/example/doit/SplashActivity.kt
+++ app/src/main/java/com/example/doit/SplashActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
-
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,3 +18,4 @@ class SplashActivity : AppCompatActivity() {
         }, 2000)
     }
 }
+
